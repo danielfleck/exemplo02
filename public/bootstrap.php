6fdd808dbe4db885d60267c03f8618126c979e45
Daniel Rodrigo Fleck <fleck@pm.pr.gov.br>
2014-01-18 19:28:04 -0200

    Configura phpunit

---
+++ public/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+date_default_timezone_set('America/Sao_Paulo');
+
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . "autoload.php";
+
---
+++ tests/aTest.php
@@ -0,0 +1,8 @@
+<?php
+class aTest extends PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
